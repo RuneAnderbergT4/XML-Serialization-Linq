6f5e4e8c19f66281a49e5b8d0108ebd159087d9f
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-13 10:07:00 +0200

    uppgift 2

--- FilmSamling/FilmSamling/Program.cs
+++ FilmSamling/FilmSamling/Program.cs
@@ -13,16 +13,23 @@ namespace FilmSamling
         static void Main(string[] args)
         {
             MovieCollection mc = ObjectXmlSerializer<MovieCollection>.Load("moviecollection.xml");
-            Console.WriteLine("Laddade " + mc.Movies.Count + " titlar");
+            //Console.WriteLine("Laddade " + mc.Movies.Count + " titlar");
 
-            Console.WriteLine("Den första filmen i listan verkar vara " + mc.Movies[0].Name);
+            //Console.WriteLine("Den första filmen i listan verkar vara " + mc.Movies[0].Name);
 
-            // Lists all movies (Uppgift 1)
-            foreach (Movie t in mc.Movies)
-            {
-                Console.WriteLine(String.Format("{0,-60}", t.Name) + String.Format("{0,-10}", t.Year) + t.Rating);
-            }
-          
+            //// Lists all movies (Uppgift 1)
+            //foreach (Movie t in mc.Movies)
+            //{
+            //    Console.WriteLine(String.Format("{0,-60}", t.Name) + String.Format("{0,-10}", t.Year) + t.Rating);
+            //}
+
+            //// Sort and list top 20 movies (based on rating) (uppgift 2)
+            //List<Movie> mcTop20 = mc.Movies.OrderByDescending(o => o.Rating).ToList();
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Console.WriteLine(String.Format("{0,-60}", mcTop20[i].Name) + String.Format("{0,-10}", mcTop20[i].Year) + mcTop20[i].Rating);
+            //}
+
             Console.ReadKey();
         }
     }

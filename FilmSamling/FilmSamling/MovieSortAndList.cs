2d3694b7b8c180fbc8d988622583579f1b851187
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-13 12:06:15 +0200

    uppgift 7

---
+++ FilmSamling/FilmSamling/MovieSortAndList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmSamling
+{
+    public class MovieSortAndList
+    {
+        public MovieSortAndList(MovieCollection mc)
+        {
+            MC = mc;
+        }
+
+        private MovieCollection MC;
+
+        public void ListAll()
+        {
+            // Lists all movies (Uppgift 1)
+            foreach (Movie n in MC.Movies)
+            {
+                Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
+            }
+        }
+
+        public void ListTop20()
+        {
+            // Sort and list top 20 movies (based on rating) (uppgift 2)
+            List<Movie> moviesTop20 = MC.Movies.OrderByDescending(o => o.Rating).ToList();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(String.Format("{0,-60}", moviesTop20[i].Name) + String.Format("{0,-10}", moviesTop20[i].Year) + moviesTop20[i].Rating);
+            }
+        }
+
+        public void ListMadeIn2012()
+        {
+            // Extracts movies made 2012 and lists in order of number of votes (Uppgift 3)
+            List<Movie> movies2012 = MC.Movies.FindAll(movie => movie.Year.Equals(2012)).ToList();
+            movies2012 = movies2012.OrderByDescending(movie => movie.Votes).ToList();
+
+            foreach (var n in movies2012)
+            {
+                Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + String.Format("{0,-10}", n.Rating) + n.Votes);
+            }
+        }
+
+        public void ListAlphabeticalAll()
+        {
+            // Lists movies in alphabetical order (uppgift 4)
+            List<Movie> moviesAlphabetical = MC.Movies.OrderBy(movie => movie.Name).ToList();
+            foreach (Movie n in moviesAlphabetical)
+            {
+                Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
+            }
+        }
+
+        public void ListAlphabeticalG()
+        {
+            // Lists movies beginning with G in alphabetical order (uppgift 5)
+            List<Movie> moviesAlphabeticalG = MC.Movies.FindAll(movie => movie.Name.StartsWith("G")).ToList();
+            moviesAlphabeticalG = moviesAlphabeticalG.OrderBy(movie => movie.Name).ToList();
+            foreach (Movie n in moviesAlphabeticalG)
+            {
+                Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
+            }
+        }
+
+        public void ListContainingRing()
+        {
+            // Lists movies with name containing ring (uppgift 6)
+            List<Movie> moviesRing = MC.Movies.FindAll(movie => movie.Name.Contains("Ring") || movie.Name.Contains("ring")).ToList();
+            foreach (Movie n in moviesRing)
+            {
+                Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
+            }
+        }
+    }
+}
--- FilmSamling/FilmSamling/Program.cs
+++ FilmSamling/FilmSamling/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,54 +14,70 @@ namespace FilmSamling
         static void Main(string[] args)
         {
             MovieCollection mc = ObjectXmlSerializer<MovieCollection>.Load("moviecollection.xml");
-            //Console.WriteLine("Laddade " + mc.Movies.Count + " titlar");
+            
+            MovieSortAndList movieSortAndList = new MovieSortAndList(mc);
 
-            //Console.WriteLine("Den första filmen i listan verkar vara " + mc.Movies[0].Name);
+            while (true)
+            {
+                OutputMenu(mc);
 
-            // Lists all movies (Uppgift 1)
-            //foreach (Movie n in mc.Movies)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
-            //}
+                string input = Console.ReadLine();
 
-            //// Sort and list top 20 movies (based on rating) (uppgift 2)
-            //List<Movie> mcTop20 = mc.Movies.OrderByDescending(o => o.Rating).ToList();
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", mcTop20[i].Name) + String.Format("{0,-10}", mcTop20[i].Year) + mcTop20[i].Rating);
-            //}
+                Console.Clear();
 
-            //// Extracts movies made 2012 and lists in order of number of votes (Uppgift 3)
-            //List<Movie> mc2012 = mc.Movies.FindAll(movie => movie.Year.Equals(2012)).ToList();
-            //mc2012 = mc2012.OrderByDescending(movie => movie.Votes).ToList();
+                switch (input)
+                {
+                    case "1":
+                        movieSortAndList.ListAll();
+                        break;
+                    case "2":
+                        movieSortAndList.ListTop20();
+                        break;
+                    case "3":
+                        movieSortAndList.ListMadeIn2012();
+                        break;
+                    case "4":
+                        movieSortAndList.ListAlphabeticalAll();
+                        break;
+                    case "5":
+                        movieSortAndList.ListAlphabeticalG();
+                        break;
+                    case "6":
+                        movieSortAndList.ListContainingRing();
+                        break;
+                    case "quit":
+                        Environment.Exit(0);
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Bad input, try again!");
+                        break;
+                }
 
-            //foreach (var t in mc2012)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", t.Name) + String.Format("{0,-10}", t.Year) + String.Format("{0,-10}", t.Rating) + t.Votes);
-            //}
+                Console.WriteLine();
+                Console.WriteLine("Press any key to return to menu!");
+                Console.ReadKey();
+            }
+        }
 
-            //// Lists movies in alphabetical order (uppgift 4)
-            //List<Movie> mcAlphabetical = mc.Movies.OrderBy(movie => movie.Name).ToList();
-            //foreach (Movie n in mcAlphabetical)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
-            //}
+        private static void OutputMenu(MovieCollection mc)
+        {
+            Console.Clear();
 
-            //// Lists movies in alphabetical order based on name and list the ones beginning with G (uppgift 5)
-            //List<Movie> mcAlphabeticalG = mc.Movies.FindAll(movie => movie.Name.StartsWith("G")).ToList();
-            //foreach (Movie n in mcAlphabeticalG)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
-            //}
+            Console.WriteLine("Loaded " + mc.Movies.Count + " movies");
+            Console.WriteLine();
 
-            //// Lists movies with name containing ring (uppgift 6)
-            //List<Movie> mcRing = mc.Movies.FindAll(movie => movie.Name.Contains("Ring") || movie.Name.Contains("ring")).ToList();
-            //foreach (Movie n in mcRing)
-            //{
-            //    Console.WriteLine(String.Format("{0,-60}", n.Name) + String.Format("{0,-10}", n.Year) + n.Rating);
-            //}
+            Console.WriteLine("1: List all movies");
+            Console.WriteLine("2: List top 20 movies based on rating");
+            Console.WriteLine("3: List all movies from 2012 based on number of votes");
+            Console.WriteLine("4: List all movies in alphabetical order");
+            Console.WriteLine("5: List all movies beginning with G in alphabetical order");
+            Console.WriteLine("6: List all movies containing ring in the title");
 
-            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Choose alternative by writing 1-6, exit or quit to close the program!");
         }
     }
 }

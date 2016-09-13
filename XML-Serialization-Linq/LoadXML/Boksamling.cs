22044131f7af83d174c5315627ea4b21272972da
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-13 14:41:05 +0200

    added failed LoadXML things

--- XML-Serialization-Linq/LoadXML/Boksamling.cs
+++ XML-Serialization-Linq/LoadXML/Boksamling.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace LoadXML
 {
-    class Boksamling
+    public class BokSamling
     {
+        public List<Bok> boksamling = new List<Bok>();
+    }
+    
+    public class Bok
+    {
+        [XmlAttribute("sprak")] public string Language = "språk";
+        [XmlElement("titel")] public string Title = "titel";
+        [XmlElement("forfattare")] public string Author = "författare";
+        
+        public Bok()
+        {
+            
+        }
     }
 }
--- XML-Serialization-Linq/LoadXML/Program.cs
+++ XML-Serialization-Linq/LoadXML/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XMLSerialisering;
 
 namespace LoadXML
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) { 
+        //{
+        //    Bok bok = new Bok();
+        //    ObjectXmlSerializer<Bok>.Save(bok, "testbok.xml");
 
+            BokSamling bs = new BokSamling();
+            bs.boksamling.Add( new Bok());
+            ObjectXmlSerializer<BokSamling>.Save(bs,"testboksamling.xml");
+
+            //BokSamling bs = ObjectXmlSerializer<BokSamling>.Load("boksamling.xml");
         }
     }
 }
